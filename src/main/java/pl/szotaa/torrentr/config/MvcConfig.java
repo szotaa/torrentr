67e379dbb57d82cd517fa2b79dfb2e70ab1d5b35
szotaa <shoothas@gmail.com>
2018-05-21 12:24:16 +0200

    initial frontend

--- src/main/java/pl/szotaa/torrentr/config/MvcConfig.java
+++ src/main/java/pl/szotaa/torrentr/config/MvcConfig.java
@@ -3,18 +3,32 @@ package pl.szotaa.torrentr.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * This class overrides default Spring MVC configuration.
  *
  * @author Jakub Szota
- *
  */
 
 @EnableWebMvc
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+    /**
+     * Configures simple automated controllers.
+     */
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry
+                .addViewController("/").setViewName("index");
+    }
+
+    /**
+     * Adds handlers to serve static resources.
+     */
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
