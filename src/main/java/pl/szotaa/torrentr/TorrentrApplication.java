8f4a76d816954e773fb3c179e53f283dab6cc3a8
szotaa <shoothas@gmail.com>
2018-05-21 10:08:18 +0200

    initial commit

---
+++ src/main/java/pl/szotaa/torrentr/TorrentrApplication.java
@@ -0,0 +1,12 @@
+package pl.szotaa.torrentr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorrentrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorrentrApplication.class, args);
+	}
+}
