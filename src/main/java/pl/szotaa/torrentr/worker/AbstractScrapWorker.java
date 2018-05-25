6852b6983a54e766e4fcc91c5e526e10fcc1b8ff
szotaa <shoothas@gmail.com>
2018-05-25 16:22:58 +0200

    add scrap worker abstraction

---
+++ src/main/java/pl/szotaa/torrentr/worker/AbstractScrapWorker.java
@@ -0,0 +1,45 @@
+package pl.szotaa.torrentr.worker;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import pl.szotaa.torrentr.domain.Result;
+
+import java.util.Set;
+import java.util.concurrent.Callable;
+
+/**
+ * Abstract class representing torrent search engine scraping task. Each implementing class should represent
+ * scrap job of one and only one search engine.
+ *
+ * @author Jakub Szota
+ */
+
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+public abstract class AbstractScrapWorker implements Callable<Set<Result>> {
+
+    /**
+     * Search query requested by user.
+     */
+
+    protected final String searchQuery;
+
+    /**
+     * Scraps the website for search engine's query search results. Protected access level to prevent direct execution.
+     *
+     * @return Set of query search results.
+     */
+
+    protected abstract Set<Result> scrap();
+
+    /**
+     * Provides concurrent execution.
+     *
+     * @return Set of query search results.
+     * @throws Exception
+     */
+
+    @Override
+    public final Set<Result> call() throws Exception {
+        return scrap();
+    }
+}
---
+++ src/main/java/pl/szotaa/torrentr/worker/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Worker classes used to scrap particular torrent search engines.
+ */
+package pl.szotaa.torrentr.worker;
